0272da1ace1eeb969dc4da3761b81a2311745f38
Joshua Marandi <134075135+joshuaMarandi@users.noreply.github.com>
2025-01-12 00:00:00 +0300

    Update add_medicine.php

--- add_medicine.php
+++ add_medicine.php
@@ -6,7 +6,8 @@
     <meta charset="utf-8">
     <title>Add New Medicine</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-		
+
+	  
 	  <script src="bootstrap/js/jquery.min.js"></script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
