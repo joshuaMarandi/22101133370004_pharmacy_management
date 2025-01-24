9303a0820590e188b0c9c20d49ec4edf69431a64
Joshua Marandi <134075135+joshuaMarandi@users.noreply.github.com>
2025-01-24 14:34:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-	  
+  <head>  
     <meta charset="utf-8">
     <title>Pharmacy Management - Login</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
